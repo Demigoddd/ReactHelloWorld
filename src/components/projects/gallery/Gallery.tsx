019742c2067ece03bed7add9b49e73a10cb520ac
Demigoddd <armen1298@gmail.com>
2024-05-31 14:45:12 +0400

    Add currency converter project

--- src/components/projects/gallery/Gallery.tsx
+++ src/components/projects/gallery/Gallery.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Gallery: React.FC = () => {
   return (
-    <>ValueConverter</>
+    <>Gallery</>
   );
 };
--- src/components/projects/valueConverter/ValueConverter.tsx
+++ src/components/projects/valueConverter/ValueConverter.tsx
@@ -1,7 +1,92 @@
 import React from "react";
+import "./style.css";
+
+const defaultCurrencies: string[] = ['USD', 'EUR', 'AMD'];
+const defaultRates: any = {
+  AMD: 4.324904,
+  USD: 0.01113748,
+  EUR: 0.01029498,
+};
+
+const ValueConverterBlock: React.FC<any> = ({
+  inputValue,
+  onChangeInputValue,
+  currencyValue,
+  onChangeCurrencyValue
+}) => {
+  return (
+    <div className="converter-block">
+      <ul className="converter-block__currencies">
+        {defaultCurrencies.map((currency) => (
+          <li
+            key={currency}
+            onClick={() => onChangeCurrencyValue(currency)}
+            className={currencyValue === currency ? 'converter-block__currencies--active' : ''}
+          >
+            {currency}
+          </li>
+        ))}
+      </ul>
+      <input
+        className="converter-block__input"
+        onChange={(e) => onChangeInputValue(e.target.value)}
+        value={inputValue}
+        type="number"
+        placeholder={"0"}
+      />
+    </div>
+  )
+};
 
 export const ValueConverter: React.FC = () => {
+  const [fromInputValue, setFromInputValue] = React.useState<any>(1);
+  const [fromCurrencyValue, setFromCurrencyValue] = React.useState<any>('USD');
+  const [toInputValue, setToInputValue] = React.useState<any>(0);
+  const [toCurrencyValue, setToCurrencyValue] = React.useState<any>('USD');
+
+  React.useEffect(() => {
+    toInputHandler(toInputValue);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [toCurrencyValue]);
+
+  React.useEffect(() => {
+    fromInputHandler(fromInputValue);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fromCurrencyValue]);
+
+  React.useEffect(() => {
+    fromInputHandler(1);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const fromInputHandler = (value: number) => {
+    const result = (defaultRates[toCurrencyValue] / defaultRates[fromCurrencyValue]) * value;
+
+    setToInputValue(result.toFixed(3));
+    setFromInputValue(value);
+  };
+
+  const toInputHandler = (value: number) => {
+    const result = (defaultRates[fromCurrencyValue] / defaultRates[toCurrencyValue]) * value;
+
+    setFromInputValue(result.toFixed(3));
+    setToInputValue(value);
+  };
+
   return (
-    <>ValueConverter</>
+    <div className="converter-countainer">
+      <ValueConverterBlock
+        inputValue={fromInputValue}
+        onChangeInputValue={fromInputHandler}
+        currencyValue={fromCurrencyValue}
+        onChangeCurrencyValue={setFromCurrencyValue}
+      />
+      <ValueConverterBlock
+        inputValue={toInputValue}
+        onChangeInputValue={toInputHandler}
+        currencyValue={toCurrencyValue}
+        onChangeCurrencyValue={setToCurrencyValue}
+      />
+    </div>
   );
 };
